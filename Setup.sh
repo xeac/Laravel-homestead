d6c48bf54e5c42aad48eb3751139fbe907684925
xeac <45033391+xeac@users.noreply.github.com>
2019-11-19 15:00:38 +0200

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,15 @@
-# 
+# Once Ubuntu VM is ready and you are logged in, execute the following comands:
+
+
+sudo apt install php7.2-cli
+sudo apt install php7.2
+sudo apt-get install php7.2-zip
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+sudo chown -R $USER ~/.composer/
+composer global require laravel/installer
