f4594dafc291f0435b2fa817e62ceacee0f42f1d
xeac <45033391+xeac@users.noreply.github.com>
2019-11-20 17:46:52 +0200

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -25,83 +25,74 @@ sudo apt-get update && sudo apt-get upgrade
 
 #Install requiered PHP Mobules
 sudo apt install php-mbstring php-xml php-bcmath
-
-
-#sites-available config from Martin's repo
-
-#Creating Database
-
-#Creating Laravel app
-cd ~
-composer create-project --prefer-dist laravel/laravel Naposao
-
-
-
-
-
-
-#OLD
-sudo apt install php7.2
-sudo apt-get install php7.2-xml php7.2-
-
-sudo apt-get install php7.2-zip
-sudo apt install python-softlayer
-php7.2-mysql php7.2-json php7.2-curl php7.2-common php7.2-zip libapache2-mod-php7.2
-sudo apt install php-mbstring php-xml php-bcmath
-sudo mysql_secure_installation
-
-
-
-sudo apt install php-fpm php-mysql
-sudo apt-get install php7.2-gd
-sudo apt-get install php-curl
-sudo apt-get update
-sudo service apache2 restart
-
-# Install and Use Composer
-sudo apt install curl php-cli php-mbstring git unzip
-cd ~
-curl -sS https://getcomposer.org/installer -o composer-setup.php
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-composer require illuminate/html
-composer update
 sudo chown -R $USER ~/.composer/
 
+#Create laravel project
+cd ~
+composer create-project --prefer-dist laravel/laravel naposao
+cd naposao
 
-sudo apt update
-sudo apt upgrade
+# copy original .env from the site to  .env
+sudo nano .env
 
-# sudo nano /etc/nginx/sites-available/$site 
+#APP_NAME=naposao
+#APP_ENV=development
+#APP_KEY=APPLICATION_UNIQUE_KEY_DONT_COPY
+#APP_DEBUG=true
+#APP_URL=http://domain_or_IP
 
-#server {
-#        listen 80;
-#        root /var/www/html;
-#        index index.php index.html index.htm index.nginx-debian.html;
-#        server_name 40.85.97.11;
+#LOG_CHANNEL=stack
+
+#DB_CONNECTION=mysql
+#DB_HOST=127.0.0.1
+#DB_PORT=3306
+#DB_DATABASE=travel_list
+#DB_USERNAME=travel_user
+#DB_PASSWORD=password
+
+
+sudo mv ~/naposao /var/www/naposao
+sudo chown -R www-data.www-data /var/www/naposao/storage
+sudo chown -R www-data.www-data /var/www/naposao/bootstrap/cache
+sudo nano /etc/nginx/sites-available/naposao
+
+
+#sudo cat server >> {  /etc/nginx/sites-available/$dir
+#sudo cat listen 80; >> /etc/nginx/sites-available/$dir
+#    server_name server_domain_or_IP;
+#    root /var/www/naposao/public;
 #
-#        location / {
-#                try_files $uri $uri/ =404;
-#        }
+#    add_header X-Frame-Options "SAMEORIGIN";
+#    add_header X-XSS-Protection "1; mode=block";
+#    add_header X-Content-Type-Options "nosniff";
 #
-#        location ~ \.php$ {
-#                include snippets/fastcgi-php.conf;
-#                fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
-#        }
+#    index index.html index.htm index.php;
 #
-#        location ~ /\.ht {
-#                deny all;
-#        }
+#    charset utf-8;
+#
+#    location / {
+#        try_files $uri $uri/ /index.php?$query_string;
+#    }
+#
+#    location = /favicon.ico { access_log off; log_not_found off; }
+#    location = /robots.txt  { access_log off; log_not_found off; }
+#
+#    error_page 404 /index.php;
+#
+#    location ~ \.php$ {
+#        fastcgi_pass unix:/var/run/php/php7.2-fpm.sock;
+#        fastcgi_index index.php;
+#        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+#        include fastcgi_params;
+#    }
+#
+#    location ~ /\.(?!well-known).* {
+#        deny all;
+#    }
 #}
 
-# sudo ln -s /etc/nginx/sites-available/naposao.rs /etc/nginx/sites-enabled/
-#sudo unlink /etc/nginx/sites-enabled/default
-#sudo nginx -t
-#sudo systemctl reload nginx
+sudo ln -s /etc/nginx/sites-available/naposao /etc/nginx/sites-enabled/
+sudo nginx -t
+sudo systemctl reload nginx
 
 
-
-
-#Create Laravel new Project from github
-
-#sudo ln -s /etc/nginx/sites-available/$Project_name /etc/nginx/sites-enabled/
-#sudo systemctl reload nginx
